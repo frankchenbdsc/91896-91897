abf5b9f0c44d202e8538cafdd94dbe1203496dfa
frankchenbdsc <frank.chen@my.bdsc.school.nz>
2022-05-19 10:03:53 +1200

    Completed commands except filling in instructions

--- Welcome.py
+++ Welcome.py
@@ -1,21 +1,41 @@
+from distutils.command import config
 from tkinter import *
 from PIL import *
 import random
 
 window = Tk()
 window.geometry("500x350") #Resizing window
-window.title("Quiz") #title
+window.title("Language Features Quiz") #title
 window.configure(bg = "white")
 window.grid_columnconfigure(0,weight=1)
 window.grid_columnconfigure(1,weight=1)
 
 def instructions():
-    enter_details.destroy()
+    global name
+    global age
+    name = (e1.get()) #Gets the value in the entry box
+    try:
+        age = int(e2.get())
+    except ValueError:
+        enter_details.config(text = "The details entered may be incorrect, please make sure it is typed correctly")
+    if name == "":
+        enter_details.config(text = "The details entered may be incorrect, please make sure it is typed correctly")
+    if age < 0:
+        enter_details.config(text = "The details entered may be incorrect, please make sure it is typed correctly")
+    e1.destroy()
+    e2.destroy()
+    input_name.destroy()
+    input_age.destroy()
+    b_continue.destroy()
+    enter_details.config(text = "This quiz is multi-choice")
+    
 
-lb1 = Label(window, text = "Quiz", bg = "white", fg = "black", borderwidth=1, relief="solid")
+    #enter_details.destroy()
+
+lb1 = Label(window, text = "Language Features Quiz", bg = "white", fg = "black", borderwidth=1, relief="solid")
 lb1.grid(row = 0, column = 0, columnspan = 2, sticky = 'ew')
 
-enter_details = Label(window, text = "Please enter your details", height=6, bg = "white")
+enter_details = Label(window, text = "Please enter your details before participating in the quiz", height=6, bg = "white")
 enter_details.grid(row = 1, column = 0, columnspan = 2, sticky = "ew")
 
 input_name = Label(window, text = "Name:", bg = "white", fg = "black")
