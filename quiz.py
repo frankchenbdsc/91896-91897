c4e3aff214e8c1e1a3591fafdb16dda62d06584e
frankchenbdsc <frank.chen@my.bdsc.school.nz>
2022-06-07 21:39:17 +1200

    Finished functionality of quiz

--- quiz.py
+++ quiz.py
@@ -12,27 +12,35 @@ window.grid_columnconfigure(0,weight=1) #Sets how much the columns in the GUI wi
 window.grid_columnconfigure(1,weight=1)
 
 class ButtonClass: #Sets up a class called ButtonClass
-    def __init__(self, name, version): #This function is called when the class is called and creates a button depending on the arguments provided
+    def buttoncreate(self, name, version): #This function is called when the class is called and creates a button depending on the arguments provided
         self.name = name #These set the arguments provided to variables that can be accessed by the entire class
         self.version = version
-        if self.name == "Continue" and self.version == 1: #Each of these will create a button that has a different command
-            self.button = Button(window, text=self.name, command=self.instructions1)
-        elif self.name == "Continue" and self.version == 2:
-            self.button = Button(window, text=self.name, command=self.instructions2)
-        else:
-            self.button = Button(window, text=self.name, command=self.choices)
+        if self.name == "Continue":
+            if self.version == 1: #Each of these will create a button that has a different command
+                self.button = Button(window, text=self.name, command=self.instructions1)
+            elif self.version == 2:
+                self.button = Button(window, text=self.name, command=self.instructions2)
+            elif self.version == 3:
+                self.button = Button(window, text=self.name, command=self.instructions3)
+        elif self.name == "Button":
+            if self.version == "A":
+                self.button_a = Button(window, text=self.version, command=lambda:self.choices("A"))
+            elif self.version == "B":
+                self.button_b = Button(window, text=self.version, command=lambda:self.choices("B"))
+            elif self.version == "C":
+                self.button_c = Button(window, text=self.version, command=lambda:self.choices("C"))
+            elif self.version == "D":
+                self.button_d = Button(window, text=self.version, command=lambda:self.choices("D"))
 
     def instructions1(self): #This function is called when a button is pressed
-        global name
-        global age
-        name = (e1.get()) #Gets the value in the entry box
+        self.name = (e1.get()) #Gets the value in the entry box
         try: #it sees if the values entered are valid, if so it then displays the instructions
-            age = int(e2.get())
+            self.age = int(e2.get())
         except ValueError:
             enter_details.config(text = "The details entered may be incorrect, please make sure it is typed correctly") #These change an exisiting label's text to the error message
-        if name == "":
+        if self.name == "":
             enter_details.config(text = "The details entered may be incorrect, please make sure it is typed correctly")
-        if age < 0:
+        if self.age < 0:
             enter_details.config(text = "The details entered may be incorrect, please make sure it is typed correctly")
         else:
             b_continue2 = ButtonClass("Continue", 2) #This creates another object that will lead to the quiz part of the program
@@ -41,54 +49,57 @@ class ButtonClass: #Sets up a class called ButtonClass
     def instructions2(self): #This chooses a technique from the list, displays the question for the technique, creates and places buttons on the window that can be clicked as the answer
         self.button.destroy()
         self.question = random.choice(question_storage.qlist)
-        global questions
-        global a
-        global b
-        global c
-        global d
-        questions = Label(window,text=question_storage.qdict[self.question], bg="white")
-        questions.grid(row=1, column=0)
-        a = ButtonClass("A", self.question)
-        a.button.grid(row=2, column=0)
-        b = ButtonClass("B", self.question)
-        b.button.grid(row=3, column=0)
-        c = ButtonClass("C", self.question)
-        c.button.grid(row=4, column=0)
-        d = ButtonClass("D", self.question)
-        d.button.grid(row=5, column=0)
+        self.questions = Label(window,text=question_storage.qdict[self.question], bg="white")
+        self.questions.grid(row=1, column=0)
+        self.buttoncreate("Button", "A")
+        self.button_a.grid(row=2, column=0)
+        self.buttoncreate("Button", "B")
+        self.button_b.grid(row=3, column=0)
+        self.buttoncreate("Button", "C")
+        self.button_c.grid(row=4, column=0)
+        self.buttoncreate("Button", "D")
+        self.button_d.grid(row=5, column=0)
 
-    def choices(self): #This is the function for each of the mutli-choice buttons, when clicked it will see if the answer matches with the button pressed, the button then displays whether it was correct, destroys itself and creates another button
-        self.question = self.version
-        global questions
-        global a
-        global b
-        global c
-        global d
-        if self.name == question_storage.adict[self.question]: #When button pressed is incorrect, displays incorrect message and clears window
+    def choices(self, choice): #This is the function for each of the mutli-choice buttons, when clicked it will see if the answer matches with the button pressed, the button then displays whether it was correct, destroys itself and creates another button
+        if choice == question_storage.adict[self.question]: #When button pressed is incorrect, displays incorrect message and clears window
             self.response = Label(window,text="correct", bg="white")
             self.response.grid(row=5, column=0)
-            questions.destroy()
-            a.button.destroy()
-            b.button.destroy()
-            c.button.destroy()
-            d.button.destroy()
-            x = ButtonClass("Continue", 2)
-            x.button.grid(row=6, column=0)
+            self.questions.destroy()
+            self.button_a.destroy()
+            self.button_b.destroy()
+            self.button_c.destroy()
+            self.button_d.destroy()
+            self.buttoncreate("Continue", 3)
+            self.button.grid(row=0, column=0) #Places the button on the window
 
         else: #When button pressed is incorrect, displays incorrect message and clears window
             self.response = Label(window,text="incorrect", bg="white")
             self.response.grid(row=5, column=0)
-            questions.destroy()
-            a.button.destroy()
-            b.button.destroy()
-            c.button.destroy()
-            d.button.destroy()
-            x = ButtonClass("Continue", 2)
-            x.button.grid(row=6, column=0)
+            self.questions.destroy()
+            self.button_a.destroy()
+            self.button_b.destroy()
+            self.button_c.destroy()
+            self.button_d.destroy()
+            self.buttoncreate("Continue", 3)
+            self.button.grid(row=0, column=0) #Places the button on the window
 
+    def instructions3(self): #This chooses a technique from the list, displays the question for the technique, creates and places buttons on the window that can be clicked as the answer
+        self.response.destroy()
+        self.button.destroy()
+        self.question = random.choice(question_storage.qlist)
+        self.questions = Label(window,text=question_storage.qdict[self.question], bg="white")
+        self.questions.grid(row=1, column=0)
+        self.buttoncreate("Button", "A")
+        self.button_a.grid(row=2, column=0)
+        self.buttoncreate("Button", "B")
+        self.button_b.grid(row=3, column=0)
+        self.buttoncreate("Button", "C")
+        self.button_c.grid(row=4, column=0)
+        self.buttoncreate("Button", "D")
+        self.button_d.grid(row=5, column=0)
 
-
-x = ButtonClass("Continue", 2) #Calls the class and creates a object and button belonging to the object
+x = ButtonClass() #Creates a class
+x.buttoncreate("Continue", 2)
 x.button.grid(row=0, column=0) #Places the button on the window
 
 
